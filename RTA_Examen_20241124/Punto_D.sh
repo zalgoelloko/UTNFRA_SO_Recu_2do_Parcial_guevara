48b37fb0b9b38c84be404e037936eea0f7858040
zalgoelloko <niko_geniall@hotmail.com>
2024-11-26 14:40:40 +0000

    ADD:Agregue el punto D

---
+++ RTA_Examen_20241124/Punto_D.sh
@@ -0,0 +1,8 @@
+"-Para crear los roles necesarios tuve que ir a /UTNFRA_SO_Recu_2do_Parcial_guevara/202411/ansible/roles
+-El comando que use para crearlos fue: ansible-galaxy role init 'nombre del rol'
+-Luego para darle una tarea a realizar modifique el archivo main.yml el cual esta en 'nombre_del_rol'/task
+-Y finalmente agregue los nombres de los roles que cree
+
+
+
+"
