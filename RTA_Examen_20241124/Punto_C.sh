6d417b4035e2129cd3843f014cd81ad7516fc81f
zalgoelloko <niko_geniall@hotmail.com>
2024-11-26 12:39:08 +0000

    ADD: Realice el  punto C del parcial

---
+++ RTA_Examen_20241124/Punto_C.sh
@@ -0,0 +1,8 @@
+"-Modifique el archivo 'index.htl' con los datos pedidos.
+-Cree la imagen docker usando el comando docker build -t zalgoelloko/web2-guevara:latest .
+-Corri la imagen usando el comando docker run -d -p 80:80 zalgoelloko/web2-guevara
+-Subi la misma a https://hub.docker.com/
+-modifique el archivo info.txt usando la ruta  web/file/
+-En docker-compose.yml modifico el archivo para levantar la imagen subida.
+-Ejecuto el archivo con docker compose up -d
+"
